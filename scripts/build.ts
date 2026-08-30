6d7714b4a9ee5e771281815e0194563adf98f83b
zohayrslileh <zohayrslileh@gmail.com>
2026-08-30 20:06:39 +0100

    Build Setup with production React runtime

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,5 +1,8 @@
 import { rm, writeFile } from "node:fs/promises"
-import { build } from "vite"
+
+process.env.NODE_ENV = "production"
+
+const { build } = await import("vite")
 
 await rm("dist", { recursive: true, force: true })
 
